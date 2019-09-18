0476437ac9dcd62f3355237f66bfafda1d254528
asciishell <asciishell@yandex.ru>
2019-09-18 23:54:58 +0300

    3 tasks

---
+++ Lab_02/code/var_list.sh
@@ -0,0 +1,28 @@
+$ set
+BLOCKSIZE=K
+EDITOR=vi
+ENV=/usr/home/biv172/17219/.shrc
+HOME=/usr/home/biv172/17219
+IFS='
+'
+LANG=ru_RU.KOI8-R
+LOGNAME=17219
+MAIL=/var/mail/17219
+MM_CHARSET=KOI8-R
+OPTIND=1
+PAGER=more
+PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/games:/usr/local/sbin:/usr/local/bin:/usr/home/biv172/17219/bin
+PPID=83268
+PS1='$ '
+PS2='> '
+PS4='+ '
+PWD=/usr/home/biv172/17219
+SHELL=/bin/sh
+SSH_CLIENT='91.228.178.74 7586 8956'
+SSH_CONNECTION='91.228.178.74 7586 92.242.57.83 8956'
+SSH_TTY=/dev/pts/2
+TERM=xterm
+USER=17219
+_=set
+var1=123
+var2='some text'
